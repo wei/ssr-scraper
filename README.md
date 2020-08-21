8bc5c732c98391f6af61425476116ddc1fa89b1b
Wei He <git@weispot.com>
2020-08-21 09:30:06 -0400

    feat: add stats

---
+++ README.md
@@ -0,0 +1,11 @@
+# SSR-Scraper
+
+![Reachable badge](https://img.shields.io/badge/dynamic/json?color=success&label=reachable&query=%24.reachableCount&url=https%3A%2F%2Fraw.githack.com%2Fwei%2Fssr-scraper%2Fmaster%2Fstats.json)
+![Reachable ssr badge](https://img.shields.io/badge/dynamic/json?color=success&label=ssr&query=%24.reachable.ssr&url=https%3A%2F%2Fraw.githack.com%2Fwei%2Fssr-scraper%2Fmaster%2Fstats.json)
+![Reachable vmess badge](https://img.shields.io/badge/dynamic/json?color=success&label=vmess&query=%24.reachable.vmess&url=https%3A%2F%2Fraw.githack.com%2Fwei%2Fssr-scraper%2Fmaster%2Fstats.json)
+![Reachable ss badge](https://img.shields.io/badge/dynamic/json?color=success&label=ss&query=%24.reachable.ss&url=https%3A%2F%2Fraw.githack.com%2Fwei%2Fssr-scraper%2Fmaster%2Fstats.json)
+![Reachable trojan badge](https://img.shields.io/badge/dynamic/json?color=success&label=trojan&query=%24.reachable.trojan&url=https%3A%2F%2Fraw.githack.com%2Fwei%2Fssr-scraper%2Fmaster%2Fstats.json)
+![Reachable snell badge](https://img.shields.io/badge/dynamic/json?color=success&label=snell&query=%24.reachable.snell&url=https%3A%2F%2Fraw.githack.com%2Fwei%2Fssr-scraper%2Fmaster%2Fstats.json)
+
+
+Scrapes all [`devtip/ssr_subscrible_tool`](https://github.com/devtip/ssr_subscrible_tool) forks to find good servers. Updated daily!
--- index.js
+++ index.js
@@ -4,138 +4,21 @@ if (!process.env.GITHUB_TOKEN) {
 }
 
 const fs = require('fs')
-const readline = require('readline')
-const Octokit = require('./lib/app-octokit')
-const fetch = require('node-fetch')
-const neek = require('neek')
-const isPortReachable = require('is-port-reachable')
+
+const fetchNodes = require('./lib/fetch-nodes')
+const dedupeFile = require('./lib/dedupe-file')
+const checkReachability = require('./lib/check-reachability')
 
 const NODES_FILE = 'nodes.txt'
 const WORKING_FILE = 'pending.tmp'
+const STATS_FILE = 'stats.json'
 
 main()
 
 async function main () {
-  await fetchNodes()
-  await dedupeNodes()
-  await checkConnectivity()
-}
-
-async function fetchNodes () {
-  const octokit = new Octokit({ auth: process.env.GITHUB_TOKEN })
-
-  console.log('Fetching forks...')
-  const forks = await octokit.paginate(
-    'GET /repos/devtip/ssr_subscrible_tool/forks',
-    {
-      per_page: 100
-    },
-    (response) =>
-      response.data.map((fork) => {
-        const {
-          full_name,
-          default_branch
-        } = fork
-
-        return {
-          full_name,
-          default_branch
-        }
-      })
-  )
-
-  const nodesFileStream = fs.createWriteStream(NODES_FILE, { flags: 'a' })
-  for (const { full_name, default_branch } of forks) {
-    try {
-      console.log(`Fetching nodes from ${full_name}`)
-      const resp = await fetch(`https://raw.githubusercontent.com/${full_name}/${default_branch}/node.txt`)
-      const data = await resp.text()
-      for (const line of data.split('\n')) {
-        if (line && line.includes('://')) { nodesFileStream.write(`${line.replace(/\s+/, '\n').trim()}\n`) }
-      }
-    } catch (e) {
-      console.warn(`Failed to fetch nodes from ${full_name}`, e)
-    }
-  }
-
-  nodesFileStream.end()
-}
-
-let uniqueLines = 0
-
-async function dedupeNodes () {
-  await (new Promise((resolve) => {
-    neek.unique(NODES_FILE, WORKING_FILE, function (result) {
-      console.log(result)
-      uniqueLines = result.unique
-      resolve()
-    })
-  }))
-}
-
-async function checkConnectivity () {
-  const nodesFileStream = fs.createWriteStream(NODES_FILE, { flags: 'w' })
-  const workingFileInterface = readline.createInterface({
-    input: fs.createReadStream(WORKING_FILE)
-  })
-
-  let counter = 0
-  workingFileInterface.on('line', async (line) => {
-    try {
-      const split = (line || '').replace(/[?#].*/g, '').split('://')
-      if (split.length === 2) {
-        const decodedConnectionString = Buffer.from(split[1], 'base64').toString('utf-8')
-        let host = ''
-        let port = ''
-
-        if (split[1].includes('@')) {
-          const components = split[1].match(/@([^:]+):(\d+)/)
-          host = components && components[1]
-          port = components && components[2]
-        } else if (split[0] === 'vmess') {
-          try {
-            const vmessJson = JSON.parse(decodedConnectionString)
-            host = vmessJson.add
-            port = vmessJson.port
-          } catch (e) {
-            const components = decodedConnectionString.match(/@([^:]+):(\d+)/)
-            host = components && components[1]
-            port = components && components[2]
-          }
-        } else {
-          if (decodedConnectionString.includes('@')) {
-            const components = decodedConnectionString.match(/@([^:]+):(\d+)/)
-            host = components && components[1]
-            port = components && components[2]
-          } else {
-            const components = decodedConnectionString.split(':')
-            host = components[0]
-            port = components[1]
-          }
-        }
-
-        if (host.match(/[^A-Za-z0-9\-.]/)) {
-          throw new Error(`Host is invalid: ${host}`)
-        }
-        if (port.match(/[^\d]/)) {
-          throw new Error(`Port is invalid: ${host}`)
-        }
-
-        const isReachable = await isPortReachable(Number(port), { host, timeout: 3000 })
-        console.log(host, port, isReachable ? 'Reachable' : 'Unreachable')
-
-        if (isReachable) nodesFileStream.write(`${line}\n`)
-      }
-    } catch (e) {
-      console.warn('Error processing node', line, e)
-    }
-
-    counter++
-    if (counter === uniqueLines) {
-      nodesFileStream.end()
-      process.exit()
-    }
-
-    counter++
-  })
+  await fetchNodes(NODES_FILE)
+  await dedupeFile(NODES_FILE, WORKING_FILE)
+  const stats = await checkReachability(WORKING_FILE, NODES_FILE)
+  console.log(stats)
+  fs.writeFileSync(STATS_FILE, JSON.stringify(stats))
 }
